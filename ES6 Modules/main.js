140bc8b22ce2a83d9877d112bf0ad60308d9c14d
Sayanduary <sayanduary@gmail.com>
2025-01-31 19:08:43 +0530

    done

---
+++ ES6 Modules/main.js
@@ -0,0 +1,5 @@
+import { PI, Circumstances, getArea, getVolume } from './mathUtil.js'
+
+console.log(PI)
+const circum = Circumstances(10);
+console.log(circum)
---
+++ ES6 Modules/mathUtil.js
@@ -0,0 +1,14 @@
+export const PI = 3.14159;
+
+export const Circumstances = (radius) => {
+  return PI * radius;
+}
+
+export const getArea = (radius) => {
+  return PI * radius * radius;
+}
+
+export function getVolume(radius) {
+  return 4 * PI * radius * radius;
+}
+
